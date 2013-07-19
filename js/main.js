ed9681789cf42a2cd3defea1e5d92594d9fee643
palaa159 <palaa159@newschool.edu>
2013-07-19 00:48:00 -0400

    04

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ $(document).delegate("#menu", "pageinit", function() {
 		$('.clickForMap').hide();
 	}
 });
-
 $(document).delegate("#mapView", "pageinit", function() {
 	var map = L.map('map', {
+		center: [-73.9790415732083, 40.73271062189632],
+		zoom: 2,
 		zoomControl: false
 	});
 	var myPos = L.circle([44, 33], 10, {
@@ -14,20 +15,27 @@ $(document).delegate("#mapView", "pageinit", function() {
 		fillOpacity: 0.9
 	}).bindPopup('You are here').addTo(map);
 	L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-		minZoom: 16,
+		minZoom: 14,
 		maxZoom: 18
 	}).addTo(map);
+	// carto DB
+	var cartoURL = 'http://motf.cartodb.com/api/v2/viz/e5b060c6-f02c-11e2-b3a8-4b78298282b3/viz.json';
+	cartodb.createLayer(map, cartoURL).on('done', function(layer) {
+		map.addLayer(layer);
+	}).on('error', function() {
+		//log the error
+	});
 	// init locate
 
 	function findLocation() {
 		map.locate({
 			watch: true,
 			setView: true,
-			minZoom: 16,
+			minZoom: 14,
 			maxZoom: 18,
 			enableHighAccuracy: true,
-			maximumAge: 10000, 
-			timeout: 100000
+			maximumAge: 10000,
+			timeout: 10000
 		});
 	}
 
@@ -43,13 +51,10 @@ $(document).delegate("#mapView", "pageinit", function() {
 		alert(e.message);
 	}
 	map.on('locationerror', onLocationError);
-	
 	$('#clientLocate').click(function() {
 		findLocation();
 	});
-	
 	findLocation();
-	
 	// fix map height
 	$('#map').css({
 		'position': 'fixed',
@@ -59,9 +64,8 @@ $(document).delegate("#mapView", "pageinit", function() {
 		'height': window.innerHeight
 	});
 });
-
 $('#flip-mini').bind('change', function(event, ui) {
-	if($('#flip-mini').val() == 'on') {
+	if ($('#flip-mini').val() == 'on') {
 		alert('triggered explore mode');
 	} else {
 		alert('disable');
