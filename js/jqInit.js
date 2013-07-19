e88c9a1fa5faa2379673d320c5538161adbc8d3f
palaa159 <palaa159@newschool.edu>
2013-07-18 22:51:00 -0400

    03

--- js/jqInit.js
+++ js/jqInit.js
@@ -1,10 +1,16 @@
+if (navigator.onLine) {
+    alert('I am online');
+} else {
+	alert('Not online');
+}
+
 $(document).bind("mobileinit", function() {
-                 // Make your jQuery Mobile framework configuration changes here!
-                 /* $.mobile.defaultPageTransition = 'none'; */
-                 $.mobile.buttonMarkup.hoverDelay = 0;
-                 $.mobile.allowCrossDomainPages = true;
-                 $.mobile.loader.prototype.options.text = "loading";
-                 $.mobile.loader.prototype.options.textVisible = true;
-                 $.mobile.loader.prototype.options.theme = "a";
-                 $.mobile.loader.prototype.options.html = "";
-                 });
+	// Make your jQuery Mobile framework configuration changes here!
+	/* $.mobile.defaultPageTransition = 'none'; */
+	$.mobile.buttonMarkup.hoverDelay = 0;
+	$.mobile.allowCrossDomainPages = true;
+	$.mobile.loader.prototype.options.text = "loading";
+	$.mobile.loader.prototype.options.textVisible = true;
+	$.mobile.loader.prototype.options.theme = "a";
+	$.mobile.loader.prototype.options.html = "";
+});
