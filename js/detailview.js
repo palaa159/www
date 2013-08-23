76138b3e4e355a6ebb52070cf07bdfc915e8efbc
Apon Palanuwech <palaa159@newschool.edu>
2013-08-22 20:03:14 -0400

    add mediaelement.js

--- js/detailview.js
+++ js/detailview.js
@@ -1,11 +1,8 @@
 console.log('detailview loaded');
 $(document).delegate("#detailView", "pageshow", function() {
 	// for vimeo player
-	var iframe = $('#vimeo-player')[0];
-	var player = $f(iframe);
 	$('#backToList').click(function() {
 	console.log('back to list');
-	player.api('pause');
 	// clear detailview
 	$('#vimeo-player').attr('src', '');
 	$('#detailNodeTitle').html('');
@@ -17,15 +14,7 @@ $(document).delegate("#detailView", "pageshow", function() {
 	var WIDTH = sW - 32;
 	$('#detailNodeTitle').html(fetchedData[currData].name);
 	$('#detailNodeAddress').html(fetchedData[currData].address);
-	$('#vimeo-player').attr({
-		'width': WIDTH,
-		'height': WIDTH*9/16,
-		'src': 'http://player.vimeo.com/video/' + fetchedData[currData].url + '?api=1'
-	});
+	
 	$('#detailSeqNum').html('[' + fetchedData[currData].ID + '/' + fetchedData.length + ']');
 	$('#detailNodeDesc').html(fetchedData[currData].desc);
-
-	window.addEventListener('orientationchange', function(evt) {
-		$('body').html(evt.orientation);
-	});
 });
